addbf8b1192030a24ba0793cf8c0866daaee9333
samanxsy <samanxsy@gmail.com>
2023-08-09 19:03:51 +0200

    Firehose initial block

--- Terraform/modules/kinesis-data-firehose/main.tf
+++ Terraform/modules/kinesis-data-firehose/main.tf
@@ -1,53 +1,70 @@
 # # Kinesis Firehose
 # # # # # # # # # # #
 
-# # ROLE
-resource "aws_iam_role" "firehose_role" {
-  name = "firehose-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "firehose.amazonaws.com"
+resource "aws_kinesis_firehose_delivery_stream" "IoT_ingestion" {
+  name = "iot-sensors-data-ingestion"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn = aws_iam_role.firehose_role.arn
+    bucket_arn = var.landing_s3_bucket_arn
+
+    buffering_size = 64
+  
+    dynamic_partitioning_configuration {
+      enabled = "true"
+    }
+
+    prefix = "data/info/id" # PLACEHOLDER
+    error_output_prefix = "errors/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/!{firehose:error-output-type}/"
+
+    processing_configuration {
+      enabled = "true"
+
+      processors {
+        type = "RecordDeAggregation"
+        parameters {
+          parameter_name = "SubRecordType"
+          parameter_value = "JSON"
         }
       }
-    ]
-  })
+
+      processors {
+        type = "MetadataExtraction"
+        parameters {
+          parameter_name  = "JsonParsingEngine"
+          parameter_value = "JQ-1.6"
+      }
+      parameters {
+          parameter_name  = "MetadataExtractionQuery"
+          parameter_value = "{customer_id:.customer_id}" # # PLACEHOLDER
+        }
+      }
+    }
+  }
+  server_side_encryption {
+    enabled = "true"
+  }
 }
 
 
 # # POLICY
-resource "aws_iam_policy" "firehose_policy" {
-  name = "firehose-policy"
+data "aws_iam_policy_document" "firehose_assume_role" {
+  statement {
+    effect = "Allow"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "s3:AbortMultipartUpload",
-          "s3:GetBucketLocation",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:PutObject"
-        ],
-        Effect   = "Allow",
-        Resource = [
-          var.landing_s3_bucket_arn,
-          "${var.landing_s3_bucket_arn}/*"
-        ]
-      }
-    ]
-  })
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-
-# # ATTACHMENT
-resource "aws_iam_role_policy_attachment" "firehose_policy_attachment" {
-  policy_arn = aws_iam_policy.firehose_policy.arn
-  role       = aws_iam_role.firehose_role.name
+# # ROLE
+resource "aws_iam_role" "firehose_role" {
+  name               = "firehose_test_role"
+  assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
 }
