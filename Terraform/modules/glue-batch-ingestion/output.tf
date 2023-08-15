c02d24ee6177e4aab7e5df844a8c55337fd5654f
samanxsy <samanxsy@gmail.com>
2023-08-15 10:26:27 +0200

    Glue output modified

--- Terraform/modules/glue-batch-ingestion/output.tf
+++ Terraform/modules/glue-batch-ingestion/output.tf
@@ -5,9 +5,9 @@
 # # # # # # # # # #
 
 output "glue_catalog_table_name" {
-  value = aws_glue_catalog_table.example_table.name
+  value = aws_glue_catalog_table.glue_table.name
 }
 
 output "glue_catalog_table_arn" {
-  value = aws_glue_catalog_table.example_table.arn
+  value = aws_glue_catalog_table.glue_table.arn
 }
